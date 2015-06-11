564c6f166c94e81d102b5a4097b1f3c05f63e4e5
michaelcao <michael.cao88@gmail.com>
2015-06-10 23:31:42 -0700

    Updated Authentication and Association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :email, :presence => true, :uniqueness => true
   validates :first_name, :presence => true
   validates :last_name, :presence => true
-  validates :email, :presence => true, :uniqueness => true
 
   has_many :listings
   has_many :messages , :class_name => "Message", :foreign_key => "sender_id"
